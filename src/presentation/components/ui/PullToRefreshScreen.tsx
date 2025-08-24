1d15f77230944efd758d2e86d31ea49d5f5f347e
Guille Varela Barros <guillevarelabarros@gmail.com>
2025-08-24 11:31:09 -0300

    Pull to refresh

---
+++ src/presentation/components/ui/PullToRefreshScreen.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { RefreshControl, ScrollView } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { colors, globalStyles } from '../../../config/theme/theme';
+import { Title } from './Title';
+
+export const PullToRefreshScreen = () => {
+  const [isRefreshing, setIsRefreshing] = useState(false);
+
+  const { top } = useSafeAreaInsets();
+
+  const onRefresh = () => {
+    setIsRefreshing(true);
+
+    setTimeout(() => {
+      setIsRefreshing(false);
+    }, 3000);
+  };
+
+  return (
+    <ScrollView
+      refreshControl={
+        <RefreshControl
+          refreshing={isRefreshing}
+          progressViewOffset={top}
+          colors={[colors.primary, 'red', 'orange', 'green']}
+          onRefresh={onRefresh}
+        />
+      }
+      style={[globalStyles.mainContainer, globalStyles.globalMargin]}
+    >
+      <Title text="Pull to refresh" safe />
+    </ScrollView>
+  );
+};
