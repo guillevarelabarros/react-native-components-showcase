4fd94e5cac6735f41fb2cf46efbca88d050ba5d0
Guille Varela Barros <guillevarelabarros@gmail.com>
2025-08-26 09:02:54 -0300

    L. 197 - 199 - Infinite Scroll con Imagenes, Animated Image

---
+++ src/presentation/screens/ui/InfiniteScrollScreen.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import { View, ActivityIndicator, FlatList } from 'react-native';
+import { FadeInImage } from '../../components/ui/FadeInImage';
+import { colors } from '../../../config/theme/theme';
+
+export const InfiniteScrollScreen = () => {
+  const [numbers, setNumbers] = useState([0, 1, 2, 3, 4, 5]);
+
+  const loadMore = () => {
+    const newArray = Array.from({ length: 5 }, (_, i) => numbers.length + i);
+
+    setTimeout(() => {
+      setNumbers([...numbers, ...newArray]);
+    }, 3000);
+  };
+
+  return (
+    <View style={{ backgroundColor: 'black' }}>
+      <FlatList
+        data={numbers}
+        onEndReached={loadMore}
+        onEndReachedThreshold={0.6}
+        keyExtractor={item => item.toString()}
+        renderItem={({ item }) => <ListItem number={item} />}
+        ListFooterComponent={() => (
+          <View style={{ height: 150, justifyContent: 'center' }}>
+            <ActivityIndicator size={40} color={colors.primary} />
+          </View>
+        )}
+      />
+    </View>
+  );
+};
+
+interface ListItemProps {
+  number: number;
+}
+
+const ListItem = ({ number }: ListItemProps) => {
+  return (
+    <FadeInImage
+      uri={`https://picsum.photos/id/${number}/500/400`}
+      style={{
+        height: 400,
+        width: '100%',
+      }}
+    />
+    // <Image
+    //   source={{ uri: `https://picsum.photos/id/${number}/500/400` }}
+    // style={{
+    //   height: 400,
+    //   width: '100%'
+    // }}
+
+    // />
+  );
+};
